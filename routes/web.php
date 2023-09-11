8f3d8c9753cb30d0d2740e63ba174d5bdffe4ecf
nigelsi2023 <nigel.pi@staff-india.com>
2023-09-11 14:56:08 +0600

    Updated Dashboard

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
 
 /*==============================================================
 ======================< Administration Routes >=================
