8d7c1ad104737d1078d70f4269b11a465c97055d
nigelsi2023 <nigel.staffindia@gmail.com>
2024-08-21 17:59:44 +0600

    Attendances filtering done

--- resources/views/administration/attendance/index.blade.php
+++ resources/views/administration/attendance/index.blade.php
@@ -12,6 +12,14 @@
     <!-- DataTables css -->
     <link href="{{ asset('assets/css/custom_css/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('assets/css/custom_css/datatables/datatable.css') }}" rel="stylesheet" type="text/css" />
+    
+    {{-- Select 2 --}}
+    <link rel="stylesheet" href="{{asset('assets/vendor/libs/select2/select2.css')}}" />
+    <link rel="stylesheet" href="{{asset('assets/vendor/libs/bootstrap-select/bootstrap-select.css')}}" />
+    
+    {{-- Bootstrap Datepicker --}}
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/bootstrap-datepicker/bootstrap-datepicker.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/bootstrap-daterangepicker/bootstrap-daterangepicker.css') }}" />
 @endsection
 
 @section('custom_css')
@@ -36,6 +44,54 @@
 @section('content')
 
 <!-- Start row -->
+<div class="row justify-content-center">
+    <div class="col-md-8">
+        <form action="{{ route('administration.attendance.index') }}" method="get" autocomplete="off">
+            <div class="card mb-4">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="mb-3 col-md-7">
+                            <label for="user_id" class="form-label">Select Employee</label>
+                            <select name="user_id" id="user_id" class="select2 form-select @error('user_id') is-invalid @enderror" data-allow-clear="true">
+                                <option value="" {{ is_null(request()->user_id) ? 'selected' : '' }}>Select Employee</option>
+                                @foreach ($users as $user)
+                                    <option value="{{ $user->id }}" {{ $user->id == request()->user_id ? 'selected' : '' }}>
+                                        {{ $user->name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                            @error('announcer_id')
+                                <b class="text-danger"><i class="feather icon-info mr-1"></i>{{ $message }}</b>
+                            @enderror
+                        </div>
+                        
+                        <div class="mb-3 col-md-5">
+                            <label class="form-label">Attendances Of</label>
+                            <input type="text" name="created_month_year" value="{{ request()->created_month_year ?? old('created_month_year') }}" class="form-control month-year-picker" placeholder="MM yyyy" tabindex="-1"/>
+                            @error('created_month_year')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>                        
+                    </div>
+                    
+                    <div class="col-md-12 text-end">
+                        @if (request()->user_id || request()->created_month_year) 
+                            <a href="{{ route('administration.attendance.index') }}" class="btn btn-danger confirm-warning">
+                                <span class="tf-icon ti ti-refresh ti-xs me-1"></span>
+                                Reset Filters
+                            </a>
+                        @endif
+                        <button type="submit" class="btn btn-primary">
+                            <span class="tf-icon ti ti-filter ti-xs me-1"></span>
+                            Filter Attendances
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </form>        
+    </div>
+</div>
+
 <div class="row">
     <div class="col-md-12">
         <div class="card mb-4">
@@ -169,11 +225,34 @@
     <script src="{{ asset('assets/js/custom_js/datatables/jquery.dataTables.min.js') }}"></script>
     <script src="{{ asset('assets/js/custom_js/datatables/dataTables.bootstrap4.min.js') }}"></script>
     <script src="{{ asset('assets/js/custom_js/datatables/datatable.js') }}"></script>
+
+    <script src="{{asset('assets/js/form-layouts.js')}}"></script>
+
+    <script src="{{asset('assets/vendor/libs/select2/select2.js')}}"></script>
+    <script src="{{asset('assets/vendor/libs/bootstrap-select/bootstrap-select.js')}}"></script>
+    
+    <script src="{{ asset('assets/vendor/libs/bootstrap-datepicker/bootstrap-datepicker.js') }}"></script>
+    <script src="{{ asset('assets/vendor/libs/bootstrap-daterangepicker/bootstrap-daterangepicker.js') }}"></script>
 @endsection
 
 @section('custom_script')
     {{--  External Custom Javascript  --}}
     <script>
         // Custom Script Here
+        $(document).ready(function() {
+            $('.bootstrap-select').each(function() {
+                if (!$(this).data('bs.select')) { // Check if it's already initialized
+                    $(this).selectpicker();
+                }
+            });
+
+            $('.month-year-picker').datepicker({
+                format: 'MM yyyy',         // Display format to show full month name and year
+                minViewMode: 'months',     // Only allow month selection
+                todayHighlight: true,
+                autoclose: true,
+                orientation: 'auto right'
+            });
+        });
     </script>
 @endsection
