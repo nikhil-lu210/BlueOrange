21ff5c1d2257219fc516bbe1a733f18d933274c4
nigelsi2023 <nigel.staffindia@gmail.com>
2024-10-07 18:55:55 +0600

    php artisan salaries:calculate
    command for auto generate salary

---
+++ app/Console/Commands/Administration/Accounts/Salary/CalculateMonthlySalaries.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Console\Commands\Administration\Accounts\Salary;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use App\Services\Administration\SalaryService\SalaryService;
+
+class CalculateMonthlySalaries extends Command
+{
+    protected $signature = 'salaries:calculate';
+
+    protected $description = 'Calculate monthly salaries for all users.';
+
+    public function handle()
+    {
+        $users = User::select('id')->whereStatus('Active')->get();
+        $salaryService = new SalaryService();
+
+        foreach ($users as $user) {
+            $salaryService->calculateMonthlySalary($user->id);
+        }
+
+        $this->info('Monthly salaries calculated successfully.');
+    }
+}
