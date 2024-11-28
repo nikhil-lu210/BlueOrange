f2765bff388a31739aca5cc6e562d3e04218885e
nigelsi2023 <nigel.staffindia@gmail.com>
2024-11-28 20:00:30 +0600

    Sidenav Shuffled

--- resources/views/layouts/administration/partials/sidenav.blade.php
+++ resources/views/layouts/administration/partials/sidenav.blade.php
@@ -207,6 +207,107 @@
             </li>
         @endcanany
 
+        <!-- Accounts -->
+        <li class="menu-header small text-uppercase">
+            <span class="menu-header-text">{{ __('Accounts') }}</span>
+        </li>
+        
+        @canany (['Salary Create', 'Salary Read'])
+            <li class="menu-item {{ request()->is('accounts/salary*') ? 'active open' : '' }}">
+                <a href="javascript:void(0);" class="menu-link menu-toggle">
+                    <i class="menu-icon tf-icons ti ti-businessplan"></i>
+                    <div data-i18n="Salary">{{ __('Salary') }}</div>
+                </a>
+                <ul class="menu-sub">
+                    <li class="menu-item {{ request()->is('accounts/salary/monthly/all*') ? 'active' : '' }}">
+                        <a href="{{ route('administration.accounts.salary.monthly.index') }}" class="menu-link">{{ __('Monthly Salaries') }}</a>
+                    </li>
+                </ul>
+            </li>
+        @endcanany
+
+        @canany (['Income Create', 'Income Read', 'Expense Create', 'Expense Read'])
+            <li class="menu-item {{ request()->is('accounts/income_expense*') ? 'active open' : '' }}">
+                <a href="javascript:void(0);" class="menu-link menu-toggle">
+                    <i class="menu-icon tf-icons ti ti-calculator"></i>
+                    <div data-i18n="Income & Expense">{{ __('Income & Expense') }}</div>
+                </a>
+                <ul class="menu-sub">
+                    <li class="menu-item {{ request()->is('accounts/income_expense/category/all*') ? 'active' : '' }}">
+                        <a href="{{ route('administration.accounts.income_expense.category.index') }}" class="menu-link">{{ __('Categories') }}</a>
+                    </li>
+
+                    @canany (['Income Create', 'Income Read'])
+                        <li class="menu-item {{ request()->is('accounts/income_expense/income*') ? 'active open' : '' }}">
+                            <a href="javascript:void(0);" class="menu-link menu-toggle">
+                                <div data-i18n="Income">{{ __('Income') }}</div>
+                            </a>
+                            <ul class="menu-sub">
+                                @can ('Income Read') 
+                                    <li class="menu-item {{ request()->is('accounts/income_expense/income/all*') ? 'active' : '' }}">
+                                        <a href="{{ route('administration.accounts.income_expense.income.index') }}" class="menu-link">
+                                            <div data-i18n="All Incomes">{{ __('All Incomes') }}</div>
+                                        </a>
+                                    </li>
+                                @endcan
+                                @can ('Income Create') 
+                                    <li class="menu-item {{ request()->is('accounts/income_expense/income/create') ? 'active' : '' }}">
+                                        <a href="{{ route('administration.accounts.income_expense.income.create') }}" class="menu-link">
+                                            <div data-i18n="Create Income">{{ __('Create Income') }}</div>
+                                        </a>
+                                    </li>
+                                @endcan
+                            </ul>
+                        </li>
+                    @endcanany
+
+                    @canany (['Expense Create', 'Expense Read'])
+                        <li class="menu-item {{ request()->is('accounts/income_expense/expense*') ? 'active open' : '' }}">
+                            <a href="javascript:void(0);" class="menu-link menu-toggle">
+                                <div data-i18n="Expense">{{ __('Expense') }}</div>
+                            </a>
+                            <ul class="menu-sub">
+                                @can ('Expense Read') 
+                                    <li class="menu-item {{ request()->is('accounts/income_expense/expense/all*') ? 'active' : '' }}">
+                                        <a href="{{ route('administration.accounts.income_expense.expense.index') }}" class="menu-link">
+                                            <div data-i18n="All Expenses">{{ __('All Expenses') }}</div>
+                                        </a>
+                                    </li>
+                                @endcan
+                                @can ('Expense Create') 
+                                    <li class="menu-item {{ request()->is('accounts/income_expense/expense/create') ? 'active' : '' }}">
+                                        <a href="{{ route('administration.accounts.income_expense.expense.create') }}" class="menu-link">
+                                            <div data-i18n="Create Expense">{{ __('Create Expense') }}</div>
+                                        </a>
+                                    </li>
+                                @endcan
+                            </ul>
+                        </li>
+                    @endcanany
+                </ul>
+            </li>
+        @endcanany
+        
+
+        <!-- Logs -->
+        @canany (['Logs Read'])
+            <li class="menu-header small text-uppercase">
+                <span class="menu-header-text">{{ __('Logs') }}</span>
+            </li>
+        
+            <li class="menu-item {{ request()->is('logs*') ? 'active open' : '' }}">
+                <a href="javascript:void(0);" class="menu-link menu-toggle">
+                    <i class="menu-icon tf-icons ti ti-history"></i>
+                    <div data-i18n="Logs">{{ __('Logs') }}</div>
+                </a>
+                <ul class="menu-sub">
+                    <li class="menu-item {{ request()->is('logs/login_logout_history*') ? 'active' : '' }}">
+                        <a href="{{ route('administration.logs.login_logout_history.index') }}" class="menu-link">{{ __('Login Histories') }}</a>
+                    </li>
+                </ul>
+            </li>
+        @endcanany
+
         <!-- Settings -->
         <li class="menu-header small text-uppercase">
             <span class="menu-header-text">{{ __('Settings') }}</span>
@@ -330,106 +431,5 @@
                 </li>
             </ul>
         </li>
-        
-        <!-- Accounts -->
-        <li class="menu-header small text-uppercase">
-            <span class="menu-header-text">{{ __('Accounts') }}</span>
-        </li>
-        
-        @canany (['Salary Create', 'Salary Read'])
-            <li class="menu-item {{ request()->is('accounts/salary*') ? 'active open' : '' }}">
-                <a href="javascript:void(0);" class="menu-link menu-toggle">
-                    <i class="menu-icon tf-icons ti ti-businessplan"></i>
-                    <div data-i18n="Salary">{{ __('Salary') }}</div>
-                </a>
-                <ul class="menu-sub">
-                    <li class="menu-item {{ request()->is('accounts/salary/monthly/all*') ? 'active' : '' }}">
-                        <a href="{{ route('administration.accounts.salary.monthly.index') }}" class="menu-link">{{ __('Monthly Salaries') }}</a>
-                    </li>
-                </ul>
-            </li>
-        @endcanany
-
-        @canany (['Income Create', 'Income Read', 'Expense Create', 'Expense Read'])
-            <li class="menu-item {{ request()->is('accounts/income_expense*') ? 'active open' : '' }}">
-                <a href="javascript:void(0);" class="menu-link menu-toggle">
-                    <i class="menu-icon tf-icons ti ti-calculator"></i>
-                    <div data-i18n="Income & Expense">{{ __('Income & Expense') }}</div>
-                </a>
-                <ul class="menu-sub">
-                    <li class="menu-item {{ request()->is('accounts/income_expense/category/all*') ? 'active' : '' }}">
-                        <a href="{{ route('administration.accounts.income_expense.category.index') }}" class="menu-link">{{ __('Categories') }}</a>
-                    </li>
-
-                    @canany (['Income Create', 'Income Read'])
-                        <li class="menu-item {{ request()->is('accounts/income_expense/income*') ? 'active open' : '' }}">
-                            <a href="javascript:void(0);" class="menu-link menu-toggle">
-                                <div data-i18n="Income">{{ __('Income') }}</div>
-                            </a>
-                            <ul class="menu-sub">
-                                @can ('Income Read') 
-                                    <li class="menu-item {{ request()->is('accounts/income_expense/income/all*') ? 'active' : '' }}">
-                                        <a href="{{ route('administration.accounts.income_expense.income.index') }}" class="menu-link">
-                                            <div data-i18n="All Incomes">{{ __('All Incomes') }}</div>
-                                        </a>
-                                    </li>
-                                @endcan
-                                @can ('Income Create') 
-                                    <li class="menu-item {{ request()->is('accounts/income_expense/income/create') ? 'active' : '' }}">
-                                        <a href="{{ route('administration.accounts.income_expense.income.create') }}" class="menu-link">
-                                            <div data-i18n="Create Income">{{ __('Create Income') }}</div>
-                                        </a>
-                                    </li>
-                                @endcan
-                            </ul>
-                        </li>
-                    @endcanany
-
-                    @canany (['Expense Create', 'Expense Read'])
-                        <li class="menu-item {{ request()->is('accounts/income_expense/expense*') ? 'active open' : '' }}">
-                            <a href="javascript:void(0);" class="menu-link menu-toggle">
-                                <div data-i18n="Expense">{{ __('Expense') }}</div>
-                            </a>
-                            <ul class="menu-sub">
-                                @can ('Expense Read') 
-                                    <li class="menu-item {{ request()->is('accounts/income_expense/expense/all*') ? 'active' : '' }}">
-                                        <a href="{{ route('administration.accounts.income_expense.expense.index') }}" class="menu-link">
-                                            <div data-i18n="All Expenses">{{ __('All Expenses') }}</div>
-                                        </a>
-                                    </li>
-                                @endcan
-                                @can ('Expense Create') 
-                                    <li class="menu-item {{ request()->is('accounts/income_expense/expense/create') ? 'active' : '' }}">
-                                        <a href="{{ route('administration.accounts.income_expense.expense.create') }}" class="menu-link">
-                                            <div data-i18n="Create Expense">{{ __('Create Expense') }}</div>
-                                        </a>
-                                    </li>
-                                @endcan
-                            </ul>
-                        </li>
-                    @endcanany
-                </ul>
-            </li>
-        @endcanany
-        
-
-        <!-- Logs -->
-        @canany (['Logs Read'])
-            <li class="menu-header small text-uppercase">
-                <span class="menu-header-text">{{ __('Logs') }}</span>
-            </li>
-        
-            <li class="menu-item {{ request()->is('logs*') ? 'active open' : '' }}">
-                <a href="javascript:void(0);" class="menu-link menu-toggle">
-                    <i class="menu-icon tf-icons ti ti-history"></i>
-                    <div data-i18n="Logs">{{ __('Logs') }}</div>
-                </a>
-                <ul class="menu-sub">
-                    <li class="menu-item {{ request()->is('logs/login_logout_history*') ? 'active' : '' }}">
-                        <a href="{{ route('administration.logs.login_logout_history.index') }}" class="menu-link">{{ __('Login Histories') }}</a>
-                    </li>
-                </ul>
-            </li>
-        @endcanany
     </ul>
 </aside>
