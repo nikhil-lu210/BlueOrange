e27223ac0740dff3984aa8ef0980c2537e6a153e
Nigel Staff-India <124186672+nigelsi2023@users.noreply.github.com>
2025-01-17 18:59:52 +0600

    Feature/users import (#47)
    * route modified
    
    * View Done
    
    * sample file
    
    * Users Import (csv) done
    
    ---------
    
    Co-authored-by: nigelsi2023 <nigel.staffindia@gmail.com>

--- routes/administration/settings/user/user.php
+++ routes/administration/settings/user/user.php
@@ -1,41 +1,46 @@
 <?php
 
 use App\Http\Controllers\Administration\Settings\User\UserController;
+use App\Http\Controllers\Administration\Settings\User\UserImportController;
 use Illuminate\Support\Facades\Route;
 
 
 /* ==============================================
 ===============< User Routes >==============
 ===============================================*/
-Route::controller(UserController::class)
-        ->prefix('user')
-        ->name('user.')
-        ->group(function () {
-            Route::get('/all', 'index')->name('index')->can('User Read');
+Route::prefix('user')->name('user.')->group(function () {
+    Route::controller(UserController::class)->group(function () {
+        Route::get('/all', 'index')->name('index')->can('User Read');
 
-            Route::get('/barcode', 'allBarcodes')->name('barcode.all')->can('User Create');
-            Route::get('/barcode/download/all', 'downloadAllBarcodes')->name('barcode.all.download')->can('User Create');
+        Route::get('/barcode', 'allBarcodes')->name('barcode.all')->can('User Create');
+        Route::get('/barcode/download/all', 'downloadAllBarcodes')->name('barcode.all.download')->can('User Create');
 
-            Route::get('/create', 'create')->name('create')->can('User Create');
-            Route::post('/store', 'store')->name('store')->can('User Create');
-            Route::get('/edit/{user}', 'edit')->name('edit')->can('User Update');
-            Route::post('/update/{user}', 'update')->name('update')->can('User Update');
-            Route::get('/destroy/{user}', 'destroy')->name('destroy')->can('User Delete');
+        Route::get('/create', 'create')->name('create')->can('User Create');
+        Route::post('/store', 'store')->name('store')->can('User Create');
+        Route::get('/edit/{user}', 'edit')->name('edit')->can('User Update');
+        Route::post('/update/{user}', 'update')->name('update')->can('User Update');
+        Route::get('/destroy/{user}', 'destroy')->name('destroy')->can('User Delete');
 
-            Route::get('/generate-qr-code/{user}', 'generateQrCode')->name('generate.qr.code')->can('User Create');
-            Route::get('/generate-bar-code/{user}', 'generateBarCode')->name('generate.bar.code')->can('User Create');
-            
-            Route::get('/show/{user}/profile', 'showProfile')->name('show.profile')->can('User Read');
-            Route::get('/show/{user}/attendance', 'showAttendance')->name('show.attendance')->can('User Read');
+        Route::get('/generate-qr-code/{user}', 'generateQrCode')->name('generate.qr.code')->can('User Create');
+        Route::get('/generate-bar-code/{user}', 'generateBarCode')->name('generate.bar.code')->can('User Create');
+        
+        Route::get('/show/{user}/profile', 'showProfile')->name('show.profile')->can('User Read');
+        Route::get('/show/{user}/attendance', 'showAttendance')->name('show.attendance')->can('User Read');
 
-            Route::post('/shift/{shift}/update/{user}', 'updateShift')->name('shift.update')->can('User Update');
+        Route::post('/shift/{shift}/update/{user}', 'updateShift')->name('shift.update')->can('User Update');
+    });
 
-            // user_interaction
-            include_once 'user_interaction/user_interaction.php';
+    Route::controller(UserImportController::class)->prefix('create/import')->name('import.')->group(function () {
+        Route::get('/', 'index')->name('index')->can('User Create');
+        Route::post('/upload', 'upload')->name('upload')->can('User Create');
+    });
 
-            // leave_allowed
-            include_once 'leave_allowed/leave_allowed.php';
+    // user_interaction
+    include_once 'user_interaction/user_interaction.php';
 
-            // salary
-            include_once 'salary/salary.php';
-        });
+    // leave_allowed
+    include_once 'leave_allowed/leave_allowed.php';
+
+    // salary
+    include_once 'salary/salary.php';
+});
