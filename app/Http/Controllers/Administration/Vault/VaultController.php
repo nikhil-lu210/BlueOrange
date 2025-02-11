1db024fe0ea88b0fac921638c0dbd1c01ec8d400
nigelsi2023 <nigel.staffindia@gmail.com>
2025-02-11 18:51:17 +0600

    Vault Query Updated

--- app/Http/Controllers/Administration/Vault/VaultController.php
+++ app/Http/Controllers/Administration/Vault/VaultController.php
@@ -23,7 +23,7 @@ class VaultController extends Controller
     {
         $user = auth()->user();
 
-        if ($user->hasPermissionTo('Vault Everything') || $user->hasPermissionTo('Vault Update') || $user->hasPermissionTo('Vault Delete')) {
+        if ($user->hasAnyPermission(['Vault Everything', 'Vault Update', 'Vault Delete'])) {
             $vaults = Vault::with(['creator'])->orderBy('name', 'ASC')->get();
         } else {
             $vaults = Vault::with(['creator'])
