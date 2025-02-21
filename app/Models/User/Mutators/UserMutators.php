ca9fb95c79dea625a564def712a4672dcbd1c68f
nigelsi2023 <nigel.staffindia@gmail.com>
2025-02-21 18:38:35 +0600

    UserMutator updated

--- app/Models/User/Mutators/UserMutators.php
+++ app/Models/User/Mutators/UserMutators.php
@@ -4,5 +4,19 @@ namespace App\Models\User\Mutators;
 
 trait UserMutators
 {
-    // 
+    /**
+     * Set the user's first_name with proper capitalization.
+     */
+    public function setFirstNameAttribute($value)
+    {
+        $this->attributes['first_name'] = ucfirst(strtolower($value));
+    }
+    
+    /**
+     * Set the user's first_name with proper capitalization.
+     */
+    public function setLastNameAttribute($value)
+    {
+        $this->attributes['last_name'] = ucfirst(strtolower($value));
+    }
 }
