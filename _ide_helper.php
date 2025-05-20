8e32a755a0b5a87febef435d5c513572f1a03e04
nigelsi2023 <nigel.staffindia@gmail.com>
2025-05-20 15:41:06 +0600

    Announcement Comment has been centralized

--- _ide_helper.php
+++ _ide_helper.php
@@ -23792,11 +23792,6 @@ namespace  {
     }
 
 
-namespace Facades\Livewire\Features\SupportFileUploads {
-    /**
-     * @mixin \Livewire\Features\SupportFileUploads\GenerateSignedUploadUrl     */
-    class GenerateSignedUploadUrl extends \Livewire\Features\SupportFileUploads\GenerateSignedUploadUrl {}
-}
 
 
 
