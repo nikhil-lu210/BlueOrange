169bd0e227d6ef62adbcc286f04a4ed4b63f6c4d
nigelsi2023 <nigel.staffindia@gmail.com>
2025-06-25 19:16:54 +0600

    Updated Public Test Page

--- app/Http/Controllers/Application/Quiz/QuizController.php
+++ app/Http/Controllers/Application/Quiz/QuizController.php
@@ -2,11 +2,22 @@
 
 namespace App\Http\Controllers\Application\Quiz;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class QuizController extends Controller
 {
+    public function __construct()
+    {
+        // Abort with 403 if user is authenticated
+        $this->middleware(function ($request, $next) {
+            if (auth()->check()) {
+                abort(403, 'Unauthorized action. You are not allowed to access this page.');
+            }
+            return $next($request);
+        });
+    }
+
     public function index()
     {
         return view('application.quiz.index');
