15985de92efde1fb28069fe507b09bc059ef546f
scottShams <scott123.staff.india@gmail.com>
2025-09-04 15:20:24 +0600

    create separate nav file for multi times uses.

---
+++ resources/views/administration/lifecycle/partials/nav.blade.php
@@ -0,0 +1,41 @@
+<!-- Navigation Tabs (remove badges, keep small left icons) -->
+<div class="row mb-4">
+    <div class="col-12">
+        <div class="card border-0 shadow-sm">
+            <div class="card-body py-2">
+                <ul class="nav nav-pills nav-fill gap-2 flex-column flex-sm-row">
+                    <li class="nav-item">
+                        <a class="nav-link d-flex align-items-center justify-content-center gap-2 active" href="{{ route('administration.lifecycle.index') }}">
+                            <i class="ti ti-layout fs-6"></i>
+                            <span>Overview</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link d-flex align-items-center justify-content-center gap-2" href="{{route('administration.lifecycle.onboarding')}}">
+                            <i class="ti ti-user-check fs-6"></i>
+                            <span>Onboarding</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link d-flex align-items-center justify-content-center gap-2" href="#active-employees">
+                            <i class="ti ti-users-plus fs-6"></i>
+                            <span>Active</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link d-flex align-items-center justify-content-center gap-2" href="#offboarding">
+                            <i class="ti ti-user-minus fs-6"></i>
+                            <span>Offboarding</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link d-flex align-items-center justify-content-center gap-2" href="#transfers">
+                            <i class="ti ti-arrow-big-right-line"></i>
+                            <span>Transfers</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
