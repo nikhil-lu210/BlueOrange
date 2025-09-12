6700d534f3e3e3b3b894af3e40c5c275cb09bfee
scottShams <scott123.staff.india@gmail.com>
2025-09-12 18:07:32 +0600

    add google translator package api for translate all data.

--- app/Http/Middleware/Localization.php
+++ app/Http/Middleware/Localization.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Stichoza\GoogleTranslate\GoogleTranslate;
 
 class Localization
 {
@@ -15,8 +16,10 @@ class Localization
      */
     public function handle(Request $request, Closure $next): Response
     {
-        app()->setLocale(session('localization', config('app.locale')));
+        $locale = session('localization', config('app.locale'));
+        app()->setLocale($locale);
 
+        view()->share('autoTranslator', new GoogleTranslate($locale));
         return $next($request);
     }
 }
