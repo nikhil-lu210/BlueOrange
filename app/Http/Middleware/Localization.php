ee4c89d97a2a4732bc4a3b6ab66166a524303dbd
scottShams <scott123.staff.india@gmail.com>
2025-09-12 21:00:03 +0600

    remove translator from middleware and use it at service class

--- app/Http/Middleware/Localization.php
+++ app/Http/Middleware/Localization.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Middleware;
 
+use App\Services\Administration\Translator\TranslatorService;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Stichoza\GoogleTranslate\GoogleTranslate;
 
 class Localization
 {
@@ -19,7 +19,7 @@ class Localization
         $locale = session('localization', config('app.locale'));
         app()->setLocale($locale);
 
-        view()->share('autoTranslator', new GoogleTranslate($locale));
+        // view()->share('autoTranslator', new TranslatorService($locale));
         return $next($request);
     }
 }
