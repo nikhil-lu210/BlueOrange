4c3c31034f1f609f411fb817f779ffd8a775545f
scottShams <scott123.staff.india@gmail.com>
2025-11-07 22:05:16 +0600

    Change the layout issue date position

--- resources/views/administration/certificate/layouts/certificate_main_layout.blade.php
+++ resources/views/administration/certificate/layouts/certificate_main_layout.blade.php
@@ -131,8 +131,10 @@
 <body>
     <section class="certificate-container">
         <div style="display: flex; justify-content: space-between; margin-bottom: 20px; font-size: 14px; color: #666;">
-            <strong style="margin-left: 25px;"> {{ $certificate->formatted_reference_no ?? 'CERT-' . ($certificate->reference_no ?? 'XXXXXXXXXX') }}</strong>
-            <strong> {{ $certificate->formatted_issue_date }}</strong>
+            <strong style="margin-left: 25px; margin-top: 3px"> {{ $certificate->formatted_reference_no ?? 'CERT-' . ($certificate->reference_no ?? 'XXXXXXXXXX') }}</strong>
+            <strong style="margin-right: -30px;">
+                {{ $certificate->formatted_issue_date }}
+            </strong>
         </div>
 
         @yield('certificate_content')
